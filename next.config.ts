813ee1721cf8890043e9a6f5cf7aba105ded6b5f
YodaheaD <yosterdaniel@gmail.com>
2026-01-05 22:12:04 -0500

    adding trailing slash to the config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',  // <-- tells Next.js to build a static export
-
+  output: "export", // <-- tells Next.js to build a static export
+  trailingSlash: true,
 };
 
 export default nextConfig;
